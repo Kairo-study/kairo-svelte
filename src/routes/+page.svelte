07e1436378352f6f58e9949df7c50ec0ddd661d4
Zayan Arshad <zayanarshad88@gmail.com>
2025-04-14 15:01:57 +0100

    feat: simple home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Bar from '$lib/components/Bar.svelte'
+</script>
+
+<Bar />
+
+<h1 class="text-center">Kairo</h1>
+
