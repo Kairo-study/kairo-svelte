b17eb9030aa67fd79f1e56a1b9e830ae286a381e
Zayan Arshad <zayanarshad88@gmail.com>
2025-04-15 18:08:57 +0100

    feat: bar icons

--- src/lib/components/Bar.svelte
+++ src/lib/components/Bar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-
+  import homeicon from '$lib/assets/home.webp?enhanced';
+  import cardsicon from '$lib/assets/cards.webp?enhanced';
 </script>
 
-<div class="bg-white flex flex-col fixed min-h-full top-0 left-0 overflow-x-hidden">
-    <a href="http://localhost:5173/">Home</a>
-    <a href="http://localhost:5173/cards">Cards</a>
+<div class="bg-white flex flex-col fixed min-h-full max-w-20 top-0 left-0 overflow-x-hidden">
+    <a href="http://localhost:5173/"><enhanced:img src={homeicon} alt="Home" /></a>
+    <a href="http://localhost:5173/cards"><enhanced:img src={cardsicon} alt="Cards" /></a>
 </div>
 
 <style>
